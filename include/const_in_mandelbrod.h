38cc5ed48dbb39fb973d3506b5f5cec1f65581b5
Alexandr26Starostin <sanchostarostin@gmail.com>
2025-03-24 22:20:02 +0300

    version 0: begin write the model of Mandelbrod

---
+++ include/const_in_mandelbrod.h
@@ -0,0 +1,9 @@
+#ifndef CONST_IN_MANDELBROD_H
+#define CONST_IN_MANDELBROD_H
+
+enum errors_in_mandelbrod
+{
+	NOT_ERROR = 0
+};
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+#include "const_in_mandelbrod.h"
+
+int main ()
+{
+	printf ("Hello!\nI am model of Mandelbrod\n");
+	return NOT_ERROR;
+}
